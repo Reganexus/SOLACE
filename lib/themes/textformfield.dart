f5e371c6ee8a62c53b15520690662871efab9f23
HyperSecond <74085079+HyperSecond@users.noreply.github.com>
2025-04-10 20:04:24 +0800

    Changed import in textformfield.dart
    -

--- lib/themes/textformfield.dart
+++ lib/themes/textformfield.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/services/text_formatter.dart';
+import 'package:flutter/services.dart';
 import 'package:solace/themes/colors.dart';
 
 class CustomTextField extends StatelessWidget {
